4bf1b535eb684dc6b7ba8ceec956af0fe590dbe5
j4rv1sgg <jrvs.inc@gmail.com>
2024-02-22 23:16:19 +0100

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Header.module.css'
+import logo from '../../assets/logo.svg'
 
 export default function Header() {
   return (
     <div className={styles.container}>
-     Header
+      <img src={logo} alt="Logo" className={styles.logo}/>
+      <span className={styles.title}>DonkeyType</span>
     </div>
   )
 }
