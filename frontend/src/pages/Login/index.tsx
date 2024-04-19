752ed7a4fb0e4e8a863478c1fa75e793085be35c
j4rv1sgg <jrvs.inc@gmail.com>
2024-04-19 13:06:58 +0200

    fix test restart

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,13 +1,18 @@
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
-import styles from './Login.module.css'
+import styles from './Login.module.css';
+import { StatusContext } from '@/context/StatusContext';
+import { StatusContextType } from '@/types/Status';
+import { useContext } from 'react';
 
 export default function Login() {
+  const [, setStatus] = useContext(StatusContext) as StatusContextType;
+  setStatus('waiting');
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <RegisterForm />
-        <LoginForm/>
+        <LoginForm />
       </div>
     </div>
   );
