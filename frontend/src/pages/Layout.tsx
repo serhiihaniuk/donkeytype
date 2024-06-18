f1445a8eb93df857e28cc967a7303bad11c2300d
j4rv1sgg <jrvs.inc@gmail.com>
2024-06-18 20:26:34 +0200

    fix footer position

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -3,16 +3,16 @@ import Header from '@/components/Header'
 import { StatusContext } from '@/context/StatusContext'
 import { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
+import styles from './Layhout.module.css'
 
 
 export default function Layout() {
-  const [status] = useContext(StatusContext)
+  const [status] = useContext(StatusContext) 
   return (
-    <>
+    <div className={styles.app}>
       <Header />
       <Outlet />
       {status === 'waiting' && <Footer />}
-      
-    </>
+    </div>
   )
 }
