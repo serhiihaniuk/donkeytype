8c2f063f03ba0daab1a126051b19804bfd22e519
j4rv1sgg <jrvs.inc@gmail.com>
2024-06-20 13:28:09 +0200

    add gh link

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,11 +1,14 @@
-import Settings from '@/pages/Home/Settings'
-import styles from './Footer.module.css'
-
+import Settings from '@/pages/Home/Settings';
+import styles from './Footer.module.css';
+import { Github } from 'lucide-react';
 
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <Settings/>
+      <a href="https://github.com/j4rv1sgg/donkeytype">
+        <Github height={27} width={27} color="var(--alt-color)" />
+      </a>
+      <Settings />
     </div>
-  )
+  );
 }
